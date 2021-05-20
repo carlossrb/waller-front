1d83a7967f7392a97d4a49751f4b67ac8d60e708
Carlos <carlos.ribeiro.brito@hotmail.com>
2021-05-20 01:43:50 -0300

    add apollo client

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,21 @@
 import ReactDOM from 'react-dom';
 import Transactions from './modules/transactions';
+import { ApolloClient, InMemoryCache } from '@apollo/client';
+import React from 'react';
+import { ApolloProvider } from '@apollo/client/react';
 
 import './styles.css';
 
+const client = new ApolloClient({
+  uri: 'localhost:4000;graphql',
+  cache: new InMemoryCache(),
+});
+
 const development = process.env.NODE_ENV === 'development';
 
-ReactDOM.render(Transactions(), document.getElementById('root'));
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <Transactions />
+  </ApolloProvider>,
+  document.getElementById('root')
+);
