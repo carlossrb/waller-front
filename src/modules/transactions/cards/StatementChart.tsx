77e81f700e6a51bfd67a74591e79ece210e59c86
Carlos <carlos.ribeiro.brito@hotmail.com>
2021-05-23 23:05:32 -0300

    create react context

--- src/modules/transactions/cards/StatementChart.tsx
+++ src/modules/transactions/cards/StatementChart.tsx
@@ -1,61 +1,66 @@
 import { ResponsivePie } from '@nivo/pie';
 import React, { memo } from 'react';
-const data = [
-  {
-    id: 'Entradas',
-    label: 'Entradas',
-    value: 400,
-    color: 'hsl(246, 70%, 50%)',
-  },
-  {
-    id: 'Saídas',
-    label: 'Saídas',
-    value: 175,
-    color: 'hsl(150, 70%, 50%)',
-  },
-];
+import { useAccountBalanceContext } from '..';
 
-export const StatementChart = memo(() => (
-  <>
-    <h2 className="text-center">Entradas e saídas</h2>
-    <ResponsivePie
-      data={data}
-      margin={{ top: 40, right: 80, bottom: 80, left: 80 }}
-      innerRadius={0.5}
-      padAngle={1}
-      cornerRadius={3}
-      activeOuterRadiusOffset={8}
-      valueFormat={(number) => String(number) + ' R$'}
-      colors={{ scheme: 'accent' }}
-      borderWidth={1}
-      borderColor={{ from: 'color', modifiers: [['darker', 0.2]] }}
-      arcLinkLabelsSkipAngle={10}
-      arcLinkLabelsTextColor={{ from: 'color', modifiers: [['brighter', 0]] }}
-      arcLinkLabelsThickness={2}
-      arcLinkLabelsColor={{ from: 'color' }}
-      arcLabelsSkipAngle={10}
-      arcLabelsTextColor={{ from: 'color', modifiers: [['darker', 10]] }}
-      theme={{
-        tooltip: { container: { color: 'white', background: '#212121' } },
-        legends: { text: { color: 'white', fill: null as any } },
-      }}
-      legends={[
-        {
-          anchor: 'bottom',
-          direction: 'row',
-          justify: false,
-          translateX: 0,
-          translateY: 40,
-          itemsSpacing: 0,
-          itemWidth: 100,
-          itemHeight: 18,
-          itemDirection: 'left-to-right',
-          itemTextColor: 'white',
-          itemOpacity: 1,
-          symbolSize: 18,
-          symbolShape: 'circle',
-        },
-      ]}
-    />
-  </>
-));
+export const StatementChart = memo(() => {
+  const { accountTotal, totalWithdrawn } = useAccountBalanceContext();
+  const data = [
+    {
+      id: 'Entradas',
+      label: 'Entradas',
+      value: accountTotal,
+      color: 'hsl(246, 70%, 50%)',
+    },
+    {
+      id: 'Saídas',
+      label: 'Saídas',
+      value: totalWithdrawn,
+      color: 'hsl(150, 70%, 50%)',
+    },
+  ];
+
+  return (
+    <>
+      <h2 className="text-center">Entradas e saídas</h2>
+      <ResponsivePie
+        data={data}
+        margin={{ top: 40, right: 80, bottom: 80, left: 80 }}
+        innerRadius={0.5}
+        padAngle={1}
+        cornerRadius={3}
+        activeOuterRadiusOffset={8}
+        valueFormat={(number) => String(number) + ' R$'}
+        colors={{ scheme: 'accent' }}
+        borderWidth={1}
+        borderColor={{ from: 'color', modifiers: [['darker', 0.2]] }}
+        arcLinkLabelsSkipAngle={10}
+        arcLinkLabelsTextColor={{ from: 'color', modifiers: [['brighter', 0]] }}
+        arcLinkLabelsThickness={2}
+        arcLinkLabelsColor={{ from: 'color' }}
+        arcLabelsSkipAngle={10}
+        arcLabelsTextColor={{ from: 'color', modifiers: [['darker', 10]] }}
+        theme={{
+          tooltip: { container: { color: 'white', background: '#212121' } },
+          legends: { text: { color: 'white', fill: null as any } },
+        }}
+        legends={[
+          {
+            anchor: 'bottom',
+            direction: 'row',
+            justify: false,
+            translateX: 0,
+            translateY: 40,
+            itemsSpacing: 0,
+            itemWidth: 100,
+            itemHeight: 18,
+            itemDirection: 'left-to-right',
+            itemTextColor: 'white',
+            itemOpacity: 1,
+            symbolSize: 18,
+            symbolShape: 'circle',
+          },
+        ]}
+      />
+    </>
+  );
+});
--- src/modules/transactions/index.tsx
+++ src/modules/transactions/index.tsx
@@ -7,7 +7,9 @@ import { Deposit } from './cards/Deposit';
 import { Payment } from './cards/Payment';
 
 import { useQuery, gql } from '@apollo/client';
+import { Query } from '../../graphql/__generated__/types';
 
+//Id fixo
 const BALANCE_QUERY = gql`
   query GetAccountBalance {
     getAccountBalance(id: "2") {
@@ -21,35 +23,85 @@ const BALANCE_QUERY = gql`
   }
 `;
 
+type AccountBalanceContextProps = {
+  userEmail?: string;
+  userName?: string;
+  accountNumber?: string;
+  accountTotal?: number;
+  totalWithdrawn?: number;
+  accountTotalNoYieldRate?: number;
+};
+
+const AccountBalanceContext = React.createContext<AccountBalanceContextProps>({
+  userEmail: '',
+  userName: '',
+  accountNumber: '',
+  accountTotal: 0,
+  totalWithdrawn: 0,
+  accountTotalNoYieldRate: 0,
+});
+
+export const useAccountBalanceContext = () => React.useContext(AccountBalanceContext);
+
 const Transactions = () => {
-  const { loading, error, data } = useQuery(BALANCE_QUERY);
-  console.log(loading, error, data);
+  const { loading, data } = useQuery<Query>(BALANCE_QUERY);
+
+  const yieldRate = () => {
+    return ((data?.getAccountBalance?.accountTotal || 0) - (data?.getAccountBalance?.accountTotalNoYieldRate || 0))
+      .toFixed(2)
+      .replace('.', ',');
+  };
+
   return (
-    <div>
-      <DeskHeader />
-      <Container>
-        <main className="h-full flex flex-col">
-          <h1 className="text-font600 font-bold text-primary400">Transações</h1>
-          <div className="flex flex-col gap-400 sm:grid sm:grid-cols-2 sm:grid-rows-2 mt-200 h-full text-primary200 font-medium text-font400">
-            <section className="h-[368px]">
-              <StatementChart />
-            </section>
-            <section className="flex flex-col">
-              <Withdraw />
-              <div className="border-gray300 border-b sm:hidden py-300" />
-            </section>
-            <section className="flex flex-col">
-              <Payment />
-              <div className="border-gray300 border-b sm:hidden py-300" />
-            </section>
-            <section className="flex flex-col">
-              <Deposit />
-            </section>
-          </div>
-        </main>
-      </Container>
-    </div>
+    <>
+      {loading ? (
+        <ScreenLoad />
+      ) : (
+        <AccountBalanceContext.Provider value={{ ...data?.getAccountBalance }}>
+          <DeskHeader />
+          <Container>
+            <div className="text-font300 text-inputMoneyColor100 flex flex-col sm:flex-row justify-between">
+              <span>
+                Olá, <strong>{data?.getAccountBalance.userName}</strong>
+              </span>
+              <span>
+                Seus rendimentos até agora: <strong>R$ {yieldRate()}</strong>
+              </span>
+            </div>
+            <main className="h-full flex flex-col">
+              <h1 className="text-font600 font-bold text-primary400">Transações</h1>
+              <div className="flex flex-col gap-400 sm:grid sm:grid-cols-2 sm:grid-rows-2 mt-200 h-full text-primary200 font-medium text-font400">
+                <section className="h-[325px]">
+                  <StatementChart />
+                </section>
+                <section className="flex flex-col">
+                  <Withdraw />
+                  <div className="border-gray300 border-b sm:hidden py-300" />
+                </section>
+                <section className="flex flex-col">
+                  <Payment />
+                  <div className="border-gray300 border-b sm:hidden py-300" />
+                </section>
+                <section className="flex flex-col">
+                  <Deposit />
+                </section>
+              </div>
+            </main>
+          </Container>
+        </AccountBalanceContext.Provider>
+      )}
+    </>
   );
 };
 
+const ScreenLoad = () => {
+  return (
+    <div className="absolute w-full h-full">
+      <div className="flex flex-col h-full w-full items-center justify-center bg-backgroundColor">
+        <div className="loader z-50" />
+        <span className="font-medium text-font500 text-inputMoneyColor">Carregando...</span>
+      </div>
+    </div>
+  );
+};
 export default Transactions;
